7abca87fab51e15bf9cc90ed71de8ad3ad4d4f68
book.huang <book.huang@ebc.net.tw>
2018-12-07 15:52:25 +0800

    新增Repository

---
+++ Activity_Demo_Repositories/Interfaces/IActivityRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Activity_Demo_Repositories.Interfaces
+{
+    interface IActivityRepository
+    {
+    }
+}
---
+++ Activity_Demo_Repositories/Repositories/ActivityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Activity_Demo_Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Activity_Demo_Repositories.Repositories
+{
+    public class ActivityRepository : IActivityRepository
+    {
+        private IDatabaseHelper _databaseHelper;
+
+        public ActivityRepository(IDatabaseHelper databaseHelper)
+        {
+            this._databaseHelper = databaseHelper;
+        }
+
+
+    }
+}
